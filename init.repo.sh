b60bef68aefd3165abaf4b877814ffdaf3d75345
jordiprats <jordi.prats@gmail.com>
2017-08-29 08:45:54 +0200

    gitkeeps

--- init.repo.sh
+++ init.repo.sh
@@ -12,10 +12,18 @@ echo "# $1" > /repo/README.md
 echo "user: $2" >> /repo/README.md
 
 mkdir -p /repo/config-catalog
+touch /repo/config-catalog/.gitkeep
 mkdir -p /repo/common
+touch /repo/common/.gitkeep
 mkdir -p /repo/environment
+touch /repo/environment/.gitkeep
 mkdir -p /repo/type
+touch /repo/type/.gitkeep
 mkdir -p /repo/type_env
+touch /repo/type_env/.gitkeep
 mkdir -p /repo/sg
+touch /repo/sg/.gitkeep
 mkdir -p /repo/sg_type_env
+touch /repo/sg_type_env/.gitkeep
 mkdir -p /repo/node
+touch /repo/node/.gitkeep
